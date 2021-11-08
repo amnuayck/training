95bd6adde7df5f20f9e0067b1074c8b29938347a
amnuayck <amnuay.katty@gmail.com>
2021-11-08 11:32:35 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-datepicker/dist/react-datepicker.css";
+import React  from "react";
+import { Route, Switch } from "react-router-dom";
 import Register from './components/Login/register'
+import Login from './components/Login/login'
 
 // import Main from './main'
 
 function App() {
   return <>
+    <React.Fragment>
+    <Switch>                
+    <Route path="/register" component={Register} />  
+    <Route path="/login" component={Login} />  
+   </Switch>   
+    </React.Fragment>
   <Register/>
   
   </>;
